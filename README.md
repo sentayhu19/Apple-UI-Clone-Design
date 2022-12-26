79929631cb5b5c4d1b3605d82c91db2a9fd5b096
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-26 21:47:13 +0300

    Update on vue config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# apple-page-ui-clone
+# apple-ui-clone
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,8 +6,8 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="text-3xl text-pink-500 font-bold">{{ msg }}</h1>
-    <h3>
+    <h1 class="green">{{ msg }}</h1>
+    <h3 class="text-orange-700">
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
