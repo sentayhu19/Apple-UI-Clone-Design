868c5ff7ff587c57d61677b335d3e9e38ff31491
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-27 00:26:50 +0300

    Mobile header color and margin updated with input box

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,25 +1,29 @@
 <template>
+    <!-- Desk -->
     <nav class="sm:hidden md:block">
-        <ul class="bg-[#333] fixed m-auto flex justify-center  gap-7 top-0 w-full h-[44px] items-center  ">
-            <li class="text-[#AEAEAE]"><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
-            <li class="text-[#AEAEAE]">Store</li>
-            <li class="text-[#AEAEAE]">Mac</li>
-            <li class="text-[#AEAEAE]">iPad</li>
-            <li class="text-[#AEAEAE]">iPhone</li>
-            <li class="text-[#AEAEAE]">Watch</li>
-            <li class="text-[#AEAEAE]">AirPods</li>
-            <li class="text-[#AEAEAE]">Tv & Home</li>
-            <li class="text-[#AEAEAE]">Only on Apple</li>
-            <li class="text-[#AEAEAE]">Accessories</li>
-            <li class="text-[#AEAEAE]">Support</li>
+        <ul class="bg-[#333] fixed m-auto flex justify-center  gap-7 top-0 w-full h-[44px] items-center pl-12 pr-12">
+            <li class="text-[#AEAEAE] "><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
+            <li class="text-[#AEAEAE] text-[12px]">Store</li>
+            <li class="text-[#AEAEAE] text-[12px]">Mac</li>
+            <li class="text-[#AEAEAE] text-[12px]">iPad</li>
+            <li class="text-[#AEAEAE] text-[12px]">iPhone</li>
+            <li class="text-[#AEAEAE] text-[12px]">Watch</li>
+            <li class="text-[#AEAEAE] text-[12px]">AirPods</li>
+            <li class="text-[#AEAEAE] text-[12px]">Tv & Home</li>
+            <li class="text-[#AEAEAE] text-[12px]">Only on Apple</li>
+            <li class="text-[#AEAEAE] text-[12px]">Accessories</li>
+            <li class="text-[#AEAEAE] text-[12px]">Support</li>
         </ul>
     </nav>
 
-        <nav class="bg-black md:hidden">
-            <div class="">
-            <p class="text-white">X</p>
-            <p class="text-white">Apple logo</p>
+        <nav class="bg-black md:hidden w-full" >
+            <div class="flex w-96 items-center h-10">
+            <p class="text-white p-5">X</p>
+            <p class="text-white m-auto">Apple logo</p>
         </div>
+    
+        <input type='text' class="bg-[#1D1D1F] w-[92%] h-[40px] m-auto ml-[12px] mr-[12px] rounded-lg"/>
+    <hr class="bg-[#AEAEAE] mt-5"/>
         <ul class="">
          <li class="text-[#AEAEAE]"><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
             <li class="text-[#AEAEAE]">Store</li>
