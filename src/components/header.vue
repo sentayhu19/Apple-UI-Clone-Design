43360ddb26d7b019663af57e95342d37d79be468
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-26 23:43:08 +0300

    Heade for mobile created

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,18 +1,38 @@
-
 <template>
-    
-        <ul class="bg-[#333] fixed m-auto  flex  gap-7 top-0 w-full ">
-            <li class="text-[#D6D6D6]"><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
-            <li class="text-[#D6D6D6]">Store</li>
-            <li class="text-[#D6D6D6]">Mac</li>
-            <li class="text-[#D6D6D6]">iPad</li>
-            <li class="text-[#D6D6D6]">iPhone</li>
-            <li class="text-[#D6D6D6]">Watch</li>
-            <li class="text-[#D6D6D6]">AirPods</li>
-            <li class="text-[#D6D6D6]">Tv & Home</li>
-            <li class="text-[#D6D6D6]">Only on Apple</li>
-            <li class="text-[#D6D6D6]">Accessories</li>
-            <li class="text-[#D6D6D6]">Support</li>
+    <nav>
+        <ul class="bg-[#333] fixed m-auto flex justify-center  gap-7 top-0 w-full h-[44px] items-center sm:hidden md:inline-flex">
+            <li class="text-[#AEAEAE]"><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
+            <li class="text-[#AEAEAE]">Store</li>
+            <li class="text-[#AEAEAE]">Mac</li>
+            <li class="text-[#AEAEAE]">iPad</li>
+            <li class="text-[#AEAEAE]">iPhone</li>
+            <li class="text-[#AEAEAE]">Watch</li>
+            <li class="text-[#AEAEAE]">AirPods</li>
+            <li class="text-[#AEAEAE]">Tv & Home</li>
+            <li class="text-[#AEAEAE]">Only on Apple</li>
+            <li class="text-[#AEAEAE]">Accessories</li>
+            <li class="text-[#AEAEAE]">Support</li>
         </ul>
+    </nav>
+
+        <nav class="bg-black">
+            <div class="">
+            <p class="text-white">X</p>
+            <p class="text-white">Apple logo</p>
+        </div>
+        <ul class="">
+         <li class="text-[#AEAEAE]"><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
+            <li class="text-[#AEAEAE]">Store</li>
+            <li class="text-[#AEAEAE]">Mac</li>
+            <li class="text-[#AEAEAE]">iPad</li>
+            <li class="text-[#AEAEAE]">iPhone</li>
+            <li class="text-[#AEAEAE]">Watch</li>
+            <li class="text-[#AEAEAE]">AirPods</li>
+            <li class="text-[#AEAEAE]">Tv & Home</li>
+            <li class="text-[#AEAEAE]">Only on Apple</li>
+            <li class="text-[#AEAEAE]">Accessories</li>
+            <li class="text-[#AEAEAE]">Support</li>
+        </ul>
+    </nav>
     
 </template>
