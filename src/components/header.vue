3691fc35bf8ba271d2f3d1ea9d6899f03dcaf017
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-26 23:20:08 +0300

    Header text color updated

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,17 +2,17 @@
 <template>
     
         <ul class="bg-[#333] fixed m-auto  flex  gap-7 top-0 w-full ">
-            <li><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
-            <li>Store</li>
-            <li>Mac</li>
-            <li>iPad</li>
-            <li>iPhone</li>
-            <li>Watch</li>
-            <li>AirPods</li>
-            <li>Tv & Home</li>
-            <li>Only on Apple</li>
-            <li>Accessories</li>
-            <li>Support</li>
+            <li class="text-[#D6D6D6]"><font-awesome-icon class="text-white-500" icon="fa-brands fa-apple" /></li>
+            <li class="text-[#D6D6D6]">Store</li>
+            <li class="text-[#D6D6D6]">Mac</li>
+            <li class="text-[#D6D6D6]">iPad</li>
+            <li class="text-[#D6D6D6]">iPhone</li>
+            <li class="text-[#D6D6D6]">Watch</li>
+            <li class="text-[#D6D6D6]">AirPods</li>
+            <li class="text-[#D6D6D6]">Tv & Home</li>
+            <li class="text-[#D6D6D6]">Only on Apple</li>
+            <li class="text-[#D6D6D6]">Accessories</li>
+            <li class="text-[#D6D6D6]">Support</li>
         </ul>
     
 </template>
