4f9b2072080adac02547979f762dfd7176a31cb9
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-28 00:32:52 +0300

    Fixed issues with hero and details

---
+++ src/components/detail.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <div class="p-8">
+            <div class="mb-4">
+        <span class="text-[32px] font-medium">Store.</span>
+    <span class="text-[#6e6e73;] text-[32px]">
+        The best way to buy the products you love.
+    </span>
+</div>
+    <div class="flex items-center mb-3">
+    <img class="rounded-full w-[35px] h-[35px] mr-3" src="../assets/needhelp.jpg"/>
+    <div class="flex flex-col">
+        <p>Need Shoping help?</p>
+        <p class="text-[#06c]">Ask a Specilalist</p>
+    </div>
+</div>
+<div class="flex items-center">
+    <img class="rounded-full w-[35px] h-[35px] mr-3" src="../assets/apple-msg-logo.png"/>
+    <div class="flex flex-col">
+        <p>Need Shoping help?</p>
+        <p class="text-[#06c;]">Find one near you ></p>
+    </div>
+</div>
+</div>
+<div class="flex max-h-[120px]  overflow-x-auto scrollbar-hide bg mt-5">
+    <div class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/mac1.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">Mac</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/iphone2.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">IPhone</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/tab3.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">iPad</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/watch4.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">Apple Watch</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/airpod5.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">AirPods</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/airtag6.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">AirTag</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/apple-tv7.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">Apple TV 4K</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/pod8.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">Home Pod mini</p>
+    </div>
+    <div  class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/acc9.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">Accessories</p>
+    </div>
+    <div class="min-w-[100px] text-center mr-2px">
+        <img src="../assets/gift10.png" class="max-h-[60px]"/>
+        <p class="pt-2 text-[14px] ">Apple Gift Card</p>
+    </div>
+</div>
+</div>
+</template>
+
+<style>
+.scrollbar-hide::-webkit-scrollbar {
+    display: none;
+}
+</style>
--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,74 +1,16 @@
 <template>
     <div>
-        <div class="p-8">
-            <div class="mb-4">
-        <span class="text-[32px] font-medium">Store.</span>
-    <span class="text-[#6e6e73;] text-[32px]">
-        The best way to buy the products you love.
-    </span>
-</div>
-    <div class="flex items-center mb-3">
-    <img class="rounded-full w-[35px] h-[35px] mr-3" src="../assets/needhelp.jpg"/>
-    <div class="flex flex-col">
-        <p>Need Shoping help?</p>
-        <p class="text-[#06c;]">Ask a Specilalist</p>
+        <div class="flex flex-col text-center">
+            <h1 class="text-[32px]">iPhone 14</h1>
+            <p class="text-[19px]">Big and bigger</p>
+            <div class="flex text-center ">
+            <p class="text-[19px] text-[#06c]">Learn more ></p>
+            <p class="text-[19px] text-[#06c]">Buy ></p>
+        </div>
+        <div>
+            <img src="../assets/iphone14-hero-sec.png" h-[548px] w-96%/>
+        </div>
+        </div>
     </div>
-</div>
-<div class="flex items-center">
-    <img class="rounded-full w-[35px] h-[35px] mr-3" src="../assets/apple-msg-logo.png"/>
-    <div class="flex flex-col">
-        <p>Need Shoping help?</p>
-        <p class="text-[#06c;]">Find one near you ></p>
-    </div>
-</div>
-</div>
-<div class="flex max-h-[120px]  overflow-x-auto scrollbar-hide bg mt-5">
-    <div class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/mac1.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">Mac</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/iphone2.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">IPhone</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/tab3.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">iPad</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/watch4.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">Apple Watch</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/airpod5.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">AirPods</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/airtag6.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">AirTag</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/apple-tv7.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">Apple TV 4K</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/pod8.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">Home Pod mini</p>
-    </div>
-    <div  class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/acc9.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">Accessories</p>
-    </div>
-    <div class="min-w-[100px] text-center mr-2px">
-        <img src="../assets/gift10.png" class="max-h-[60px]"/>
-        <p class="pt-2 text-[14px] ">Apple Gift Card</p>
-    </div>
-</div>
-</div>
+        
 </template>
-
-<style>
-.scrollbar-hide::-webkit-scrollbar {
-    display: none;
-}
-</style>
