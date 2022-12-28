e1b14fe86c627d7ac0bd2037b5b0819873c6a1b0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-28 13:46:08 +0300

    Iphone 14 section added

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -11,13 +11,45 @@
             <img src="../assets/iphone14-hero-sec.png" class="h-[450px] w-full" />
         </div>
         <div>
-            <img  src="../assets/gift-home.png" class="h-[330px] w-full absolute z-0 "/>
-        <div class="relative z-10 mt-56">          
+            <img  src="../assets/gift-home.png" class="h-[360px] w-full absolute z-0 "/>
+        <div class="relative z-10 mt-64">          
             <h2 class="text-[32px] text-[#f5f5f7]">Wow yourslef.</h2>
             <p class="text-[#f5f5f7] max-h-[240px]">Get he gift you've been eyeing all season.</p>
             <p class="text-[#06c]">Shop ></p>
         </div>
 
+        </div>
+        <div class="mt-3">
+            <div class="bg-black h-30 pt-5 ">
+                <div class="flex flex-col">
+                    <div class="flex flex-col">
+                    <h2 class="text-[#f5f5f7] text-3xl">Watch</h2>
+                    <p class="text-red-500">SERIES</p>
+                    </div>
+                  
+                    <h2 class="text-[#f5f5f7]">A healthy leap ahead.</h2>
+                    <div class="flex text-center m-auto gap-5">
+                    <p class="text-[#06c]">Learn more ></p>
+                    <p class="text-[#06c]">Buy ></p>
+                </div>
+                </div>    
+            </div>
+            <img src="../assets/whatch-home.png" class="w-full h-full"/>
+        </div>
+        <div class="mt-3">
+            <div class="bg-black h-30 pt-5 ">
+                <div class="flex flex-col">
+                    <div class="flex flex-col">
+                    <h2 class="text-[#f5f5f7] text-3xl">iPhone 14 pro</h2>
+                    <h3 class="text-[#f5f5f7]">Pro.Beyond.</h3>
+                    </div>
+                    <div class="flex text-center m-auto gap-5">
+                    <p class="text-[#06c]">Learn more ></p>
+                    <p class="text-[#06c]">Buy ></p>
+                </div>
+                </div>    
+            </div>
+            <img src="../assets/iphone-home.png" class="w-full h-full"/>
         </div>
         </div>
     </div>
