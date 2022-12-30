9b94646e5cb530790207d983312bbc4acc0068ec
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-12-30 18:30:09 +0300

    Home removed from app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Home from './components/Home.vue'
 import Header from './components/header.vue'
 import Hero from './components/hero.vue'
 import Footer from './components/footer.vue'
